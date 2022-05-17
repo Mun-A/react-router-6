b28a55c1d3c3f899efa85c880301a19afde4bab6
mun-a <karnevall.13.13@gmail.com>
2022-05-17 20:57:57 +0500

    add single product route and useParams()

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,14 @@
+import { Link, useParams } from 'react-router-dom';
+
+import products from '../data';
+
 const SingleProduct = () => {
+  const { productId } = useParams();
+
   return (
-    <section className='section product'>
-      <h2>single product</h2>
+    <section className="section product">
+      <h2>{productId}</h2>
+      <Link to="/products" className='btn'>back to products</Link>
     </section>
   );
 };
