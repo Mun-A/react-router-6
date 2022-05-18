139b4bd989b47459ba7b9d11c0a051fdf41b2f1a
mun-a <karnevall.13.13@gmail.com>
2022-05-18 10:32:15 +0500

    add useNavigate()

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -6,8 +7,12 @@ import Products from './pages/Products';
 import Error from './pages/Error';
 import SharedLayout from './pages/SharedLayout';
 import SingleProduct from './pages/SingleProduct';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
+  const [user, setUser] = useState(null);
+  
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
@@ -15,6 +20,8 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="products" element={<Products />} />
         <Route path="products/:productId" element={<SingleProduct />} />
+        <Route path="login" element={<Login setUser={setUser} />} />
+        <Route path="dashboard" element={<Dashboard user={user} />} />
         <Route path="*" element={<Error />} />
       </Route>
     </Routes>
